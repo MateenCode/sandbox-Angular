bf317535879ed6d2b266de8e55e0aa4b298eb32e
Mateen kazia <mateenkazia@Mateens-MacBook-Pro.local>
2019-01-31 14:13:43 -0800

    Loops with ngFor covered

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -7,9 +7,59 @@ import { User } from "../../models/User";
   styleUrls: ["./users.component.css"]
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+
   constructor() {}
 
-  ngOnInit() {}
-
-  onSubmit() {}
+  ngOnInit() {
+    this.users = [
+      {
+        firstName: "John",
+        lastName: "Doe",
+        email: "john@gmail.com",
+        age: 32,
+        address: {
+          street: "50 main st",
+          city: "Los Angeles",
+          state: "CA"
+        }
+      },
+      {
+        firstName: "Jimmy",
+        lastName: "foe",
+        email: "Jimmy@gmail.com",
+        age: 32,
+        address: {
+          street: "22 jump st",
+          city: "Sunnyvale",
+          state: "CA"
+        }
+      },
+      {
+        firstName: "Jenny",
+        lastName: "hoe",
+        email: "Jenny@gmail.com",
+        age: 24,
+        address: {
+          street: "16 peep st",
+          city: "Atlanta",
+          state: "GA"
+        }
+      }
+    ];
+    this.addUser({
+      firstName: "david",
+      lastName: "kendey",
+      email: "david@gmail.com",
+      age: 24,
+      address: {
+        street: "45 wake st",
+        city: "Miami",
+        state: "FL"
+      }
+    });
+  }
+  addUser(user: User) {
+    this.users.push(user);
+  }
 }
