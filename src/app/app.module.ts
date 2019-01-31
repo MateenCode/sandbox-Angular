c28c0b27062411800a8e4dd86a42abb9827461f3
Mateen kazia <mateenkazia@Mateens-MacBook-Pro.local>
2019-01-31 13:47:25 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/Forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { UserComponent } from "./components/user/user.component";
+import { UsersComponent } from "./components/users/users.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, UserComponent, UsersComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+import { User } from "../../models/User";
+
+@Component({
+  selector: "app-users",
+  templateUrl: "./users.component.html",
+  styleUrls: ["./users.component.css"]
+})
+export class UsersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSubmit() {}
+}
